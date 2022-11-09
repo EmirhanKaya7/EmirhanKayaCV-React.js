b47c5afa2c334d9ed143b5fc4ee1b57dfb7a90b1
Emirhan Kaya <aemirhan@sabanciuniv.edu>
2022-11-09 16:52:44 +0300

    hovers

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import emirhan from "../images/emirhan.png";
 function Header() {
 return(
 <header>
-<div className="top"><img className="circle-img" src={emirhan} style={{width:200, height:200} }></img>
+<div className="top"><img className="circle-img" alt="Emirhan" src={emirhan} style={{width:200, height:200} }></img>
 <div className="name">
 <h1>Emirhan Kaya 
 <p  className="inf" >Computer Science Major & Business Analytics Minor Student <br></br> Sabanci University</p>
@@ -13,7 +13,7 @@ return(
 </div>
 </div>
 </header>
-)
+)   
 
 }
 export default Header;
