c7b863aaa09e000201eedda9455c6482cb39bc9a
Emirhan Kaya <aemirhan@sabanciuniv.edu>
2022-11-09 20:32:13 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Components/Header';
-import Form from './Components/Contact';
 import App from './Components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
